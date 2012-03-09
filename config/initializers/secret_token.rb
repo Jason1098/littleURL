157eddc702eab711b2f38a73aa22045ec4c30988
Jason Bush <jasonbushis@gmail.com>
2012-03-09 14:09:04 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LittleURL::Application.config.secret_token = 'a16fa4fe815c1c5905ec9639b64c112350b7a5721c9d6a9c5a84debd518d4713a6666eaa26da4530ad4739fade47082ca16863a7a414936ac34c8b879bd1748c'
