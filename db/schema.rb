ef458ba31fbaf0f9764c4d2dda727dc026330d03
Jason Bush <jasonbushis@gmail.com>
2012-03-09 14:31:06 -0800

    views added and db migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120309222307) do
+ActiveRecord::Schema.define(:version => 20120309222634) do
+
+  create_table "urls", :force => true do |t|
+    t.string   "long"
+    t.string   "short"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
